5a2ebe01678495884b0ec4443ec4e90e391972b1
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-05-21 22:57:42 +0530

    empty pages added with content

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+    <div className="w-full h-screen bg-gradient-to-br from-black to-darkgreen text-gold flex flex-col items-center py-4">
+      
+    </div>
+    </>
+  );
+}
