7093fcb7ca8366be0df566afe0cedb5f3b90f235
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-05-24 17:19:33 +0530

    another try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/sidebar";
 import Head from 'next/head';
-// import logo from "./logo.ico";
+import logo from "./favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +19,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <link rel="icon" href={logo.src} />
+      </head>
       <body className={`flex ${inter.className}`}>
         <div className="w-1/12 fixed top-0" style={{ zIndex: 21 }}>
           <SideBar />
