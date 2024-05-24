789cb8c6d9dd784a133cca241aa84e29fd5c81e3
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-05-24 23:05:51 +0530

    new mobile responsive home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       <div className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-br from-black to-darkMaroon text-gold flex flex-col items-center">
         <Particles />
         <div
-          className="w-full h-full flex flex-col md:flex-row justift-center items-center"
+          className="w-full h-full flex flex-col md:flex-row justift-center items-center relative"
           style={{ zIndex: 21 }}
         >
-          <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
+          <div className="w-full md:w-1/3 flex flex-col justify-center items-center absolute top-[100px] md:relative md:top-auto">
             <h1 className="text-right text-white w-full">
               <b>
-                <span className="text-4xl md:text-6xl">Kasun Udara</span>
+                <span className="text-4xl md:text-6xl text-outline">Kasun Udara</span>
               </b>
             </h1>
             <div className="sm:text-1xl md:text-2xl text-right text-gold w-full">
