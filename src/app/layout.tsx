653158266df8ea36a8f32056c91afff80ef95428
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-05-24 17:00:07 +0530

    favicon added again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/sidebar";
-import logo from "./logo.ico";
+// import logo from "./logo.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href={logo.src} />
+        <link rel="icon" href="/logo.ico" />
       </head>
       <body className={`flex ${inter.className}`}>
         <div className="w-1/12 fixed top-0" style={{ zIndex: 21 }}>
