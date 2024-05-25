9337e737bb3c3b5c8ecd140f9bba39bbcf9c8407
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-05-25 10:49:04 +0530

    paragraph added for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,15 @@ export default function Home() {
           <div className="w-full md:w-1/3 flex flex-col justify-center items-center absolute top-[100px] md:relative md:top-auto">
             <h1 className="text-right text-white w-full">
               <b>
-                <span className="text-4xl md:text-6xl text-outline">Kasun Udara</span>
+                <span className="text-4xl md:text-6xl text-outline">
+                  Kasun Udara
+                </span>
               </b>
             </h1>
+            <p className="text-right text-white w-full text-sm pl-4 md:pl-0">
+              A computer science undergraduate at University of Colombo School
+              of Computing. I am also the founder at our startup IgniteX.
+            </p>
             <div className="sm:text-1xl md:text-2xl text-right text-gold w-full">
               <b>
                 <Typewriter
